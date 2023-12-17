cbecbd46dd4bc706dd66f0983c9c454d58c390d0
Francis Baidoo <kwaw.baidoo@gmail.com>
2023-12-17 10:34:03 +0000

    Expense, Employee salary validations

--- src/components/Employees/AddEmployeeSalary.jsx
+++ src/components/Employees/AddEmployeeSalary.jsx
@@ -1,4 +1,16 @@
+import { useForm } from "react-hook-form";
+
 const AddEmployeeSalary = ({ closeModal }) => {
+const {
+  register,
+  handleSubmit,
+  formState: {errors},
+} = useForm();
+
+const submitForm = (data) =>{
+  console.log(data);
+}
+
   return (
     <div className="fixed flex z-50 justify-center items-center py-4 top-0 left-0 bg-black/25 w-full h-full">
       <div className="flex flex-col p-4 bg-white w-[900px] h-auto rounded-md">
@@ -22,20 +34,27 @@ const AddEmployeeSalary = ({ closeModal }) => {
             />
           </svg>
         </div>
-        <form className="px-4 flex flex-col gap-3" action="">
+        <div className="px-4 flex flex-col gap-3" action="">
           <div className="flex flex-col  " >
             <label className="text-lg font-thin mb-2" htmlFor="salary">Salary</label>
-            <input className="border border-gray-400 px-2 h-12 outline-none rounded-md" type="text" placeholder="e.g. 500.00" />
+            <input className="border border-gray-400 px-2 h-12 outline-none rounded-md" type="number" placeholder="e.g. 500.00"
+            {...register("salary", {required: "Salary is required"})}
+            />
+            <span className="text-red-500">{errors?.salary && errors?.salary?.message}</span>
           </div>
           <div className="flex flex-col  ">
           <label className="text-lg font-thin mb-2" htmlFor="date paid">Date paid</label>
-          <input className="border border-gray-400 px-2 h-12 outline-none rounded-md" type="date" />
+          <input className="border border-gray-400 px-2 h-12 outline-none rounded-md" type="date"
+        
+          {...register("date", {required: "Date is required"})}
+          />
+          <span className="text-red-500">{errors?.date && errors?.date.message}</span>
           </div>
           <div className="flex gap-4 py-4">
             <button className="bg-slate-500 rounded-md w-36 px-4 py-2 text-lg text-white">Cancel</button>
-            <button className="bg-orange-500 rounded-md text-lg text-white px-4 py-2 w-36">Add</button>
+            <button onClick={handleSubmit(submitForm)} className="bg-orange-500 rounded-md text-lg text-white px-4 py-2 w-36">Add</button>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
--- src/components/Expenses/AddExpenses.jsx
+++ src/components/Expenses/AddExpenses.jsx
@@ -1,5 +1,15 @@
+import { useForm } from "react-hook-form";
 const AddExpenses = ({ rowData, closeModal, setRowData }) => {
-  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="fixed flex justify-center z-50 items-center py-4 top-0 left-0 bg-black/25 w-full h-full">
       <div className="bg-white w-[900px] h-auto rounded-md">
@@ -32,12 +42,21 @@ const AddExpenses = ({ rowData, closeModal, setRowData }) => {
             <label className="text-lg font-thin mb-2" htmlFor="Category">
               Category
             </label>
-            <select value={rowData?.expense_category} className="border px-2  outline-none h-12 rounded-md w-full">
+            <select
+              defaultValue={rowData?.expense_category}
+              className="border px-2  outline-none h-12 rounded-md w-full"
+              {...register("category", {
+                required: "Category is required",
+              })}
+            >
               <option value="">Choose category</option>
               <option value="Fish">Fish</option>
               <option value="Poultry">Poultry</option>
               <option value="Meat">Meat</option>
             </select>
+            <span className="text-red-500">
+              {errors?.category && errors?.category?.message}
+            </span>
           </div>
 
           <div className="grid grid-cols-2 gap-2">
@@ -46,22 +65,32 @@ const AddExpenses = ({ rowData, closeModal, setRowData }) => {
                 Reference
               </label>
               <input
-                defaultValue = {rowData?.reference || ""}
+                defaultValue={rowData?.reference || ""}
+                {...register("reference", {
+                  required: "Reference is required",
+                })}
                 className="border px-2  outline-none h-12 rounded-md w-full"
                 type="text"
                 placeholder="Reference"
               />
+              <span className="text-red-500">
+                {errors?.reference && errors?.reference?.message}
+              </span>
             </div>
             <div className="w-full ">
               <label className="text-lg font-thin mb-2" htmlFor="Date">
                 Date
               </label>
               <input
-                  value={rowData?.date}
+                defaultValue={rowData?.date}
+                {...register("date", { required: "Date is required" })}
                 className="border px-2  outline-none h-12 rounded-md w-full"
                 type="date"
                 placeholder="2023-12-12"
               />
+              <span className="text-red-500">
+                {errors?.date && errors?.date?.message}
+              </span>
             </div>
           </div>
           <div className="grid grid-cols-1 gap-2">
@@ -70,11 +99,15 @@ const AddExpenses = ({ rowData, closeModal, setRowData }) => {
                 Amount
               </label>
               <input
-                  value={rowData?.amount}
+                defaultValue={rowData?.amount || ""}
+                {...register("amount", { required: "Amount is required" })}
                 className="border px-2  outline-none h-12 rounded-md w-full"
                 type="number"
                 placeholder="e.g 300"
               />
+              <span className="text-red-500">
+                {errors?.amount && errors?.amount?.message}
+              </span>
             </div>
           </div>
           <div className="grid grid-cols-1 gap-2">
@@ -82,7 +115,17 @@ const AddExpenses = ({ rowData, closeModal, setRowData }) => {
               <label className="text-lg font-thin mb-2" htmlFor="Description">
                 Description
               </label>
-              <textarea value={rowData?.description} rows={10} className="border px-2  outline-none h-12 rounded-md w-full"></textarea>
+              <textarea
+                defaultValue={rowData?.description || ""}
+                rows={10}
+                className="border px-2  outline-none h-12 rounded-md w-full"
+                {...register("description", {
+                  required: "Description is required",
+                })}
+              ></textarea>
+              <span className="text-red-500">
+                {errors?.description && errors?.description?.message}
+              </span>
             </div>
           </div>
 
@@ -90,7 +133,10 @@ const AddExpenses = ({ rowData, closeModal, setRowData }) => {
             <button className=" bg-slate-500 px-4 py-2 w-36 text-lg text-white rounded-md ">
               Cancel
             </button>
-            <button className=" bg-orange-500 px-4 py-2 w-36 text-lg text-white rounded-md ">
+            <button
+              onClick={handleSubmit(submitForm)}
+              className=" bg-orange-500 px-4 py-2 w-36 text-lg text-white rounded-md "
+            >
               {Object.keys(rowData).length ? "Update" : "Add"}
             </button>
           </div>
