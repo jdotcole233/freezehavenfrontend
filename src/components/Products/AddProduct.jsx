4c1dc8059c757f76090d31f9e4f545d87d78c4e0
Jay Cole <jdotcole233@gmail.com>
2023-12-17 09:55:26 +0000

    Add product, product details validation

--- src/components/Products/AddProduct.jsx
+++ src/components/Products/AddProduct.jsx
@@ -1,14 +1,29 @@
+import { useForm } from "react-hook-form";
+
 const AddProduct = ({ closeModal, editProduct, setEditProduct }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitProductForm = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex transition-opacity justify-center py-8 items-center bg-black/25 z-50 fixed top-0 left-0 w-full h-screen">
       {/* Add Product Form */}
       <div className=" w-[900px] h-auto rounded-md bg-white shadow-md">
         <div className="flex justify-between py-4 px-4">
-          <h1 className="text-lg font-semibold"> {Object.keys(editProduct).length ? 'Edit': 'Add New'} Product</h1>
+          <h1 className="text-lg font-semibold">
+            {" "}
+            {Object.keys(editProduct).length ? "Edit" : "Add New"} Product
+          </h1>
           <svg
             onClick={() => {
-              closeModal(false)
-              setEditProduct({})
+              closeModal(false);
+              setEditProduct({});
             }}
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -26,17 +41,23 @@ const AddProduct = ({ closeModal, editProduct, setEditProduct }) => {
         </div>
 
         {/* Form */}
-        <form className="flex flex-col gap-3 px-4" action="">
+        <div className="flex flex-col gap-3 px-4">
           <div className="w-full ">
             <label className="text-lg font-thin mb-2" htmlFor="Product Name">
               Product Name
             </label>
             <input
-              value={editProduct.product_name ||''}
+              defaultValue={editProduct.product_name || ""}
+              {...register("product_name", {
+                required: "Product name is required",
+              })}
               className="border px-2  outline-none h-12 rounded-md w-full"
               type="text"
               placeholder="E.g. Kpanla"
             />
+            <span className="text-red-500">
+              {errors?.product_name && errors?.product_name?.message}
+            </span>
           </div>
 
           <div className="grid grid-cols-2 gap-2">
@@ -44,26 +65,44 @@ const AddProduct = ({ closeModal, editProduct, setEditProduct }) => {
               <label className="text-lg font-thin mb-2" htmlFor="Product Name">
                 Product Type
               </label>
-              <select value={editProduct.product_type ||''} className="border px-2  outline-none h-12 rounded-md w-full">
+              <select
+                defaultValue={editProduct.product_type || ""}
+                {...register("product_type", {
+                  required: "Select a product type",
+                })}
+                className="border px-2  outline-none h-12 rounded-md w-full"
+              >
                 <option value="">Choose Type</option>
                 <option value="Fish">Fish</option>
                 <option value="Poultry">Poultry</option>
                 <option value="Meat">Meat</option>
               </select>
+              <span className="text-red-500">
+                {errors?.product_type && errors?.product_type?.message}
+              </span>
             </div>
             <div className="w-full ">
               <label className="text-lg font-thin mb-2" htmlFor="Product Name">
                 Product Category
               </label>
-              <select value={editProduct.product_category || ''} className="border px-2  outline-none h-12 rounded-md w-full">
+              <select
+                defaultValue={editProduct.product_category || ""}
+                {...register("product_category", {
+                  required: "Select a product category",
+                })}
+                className="border px-2  outline-none h-12 rounded-md w-full"
+              >
                 <option value="">Choose Category</option>
                 <option value="Frozen Food">Frozen Food</option>
                 <option value="Water">Water</option>
               </select>
+              <span className="text-red-500">
+                {errors?.product_category && errors?.product_category?.message}
+              </span>
             </div>
           </div>
 
-          <div className="w-full  ">
+          {/* <div className="w-full  ">
             <label className="text-lg font-thin mb-2" htmlFor="Product Name">
               Product Image
             </label>
@@ -72,16 +111,62 @@ const AddProduct = ({ closeModal, editProduct, setEditProduct }) => {
               type="file"
               placeholder="E.g. Kpanla"
             />
+          </div> */}
+          <div class="col-span-full">
+            <label
+              for="cover-photo"
+              class="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Cover photo
+            </label>
+            <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+              <div class="text-center">
+                <svg
+                  class="mx-auto h-12 w-12 text-gray-300"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                    for="file-upload"
+                    class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Upload a file</span>
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      type="file"
+                      class="sr-only"
+                    />
+                  </label>
+                  <p class="pl-1">or drag and drop</p>
+                </div>
+                <p class="text-xs leading-5 text-gray-600">
+                  PNG, JPG, GIF up to 10MB
+                </p>
+              </div>
+            </div>
           </div>
 
           <div className="flex gap-4 py-4">
-            <button className=" bg-slate-500 px-4 py-2 w-36 text-lg text-white rounded-md ">Cancel</button>
-            <button className=" bg-orange-500 px-4 py-2 w-36 text-lg text-white rounded-md ">
-            {Object.keys(editProduct).length ? "Update" : "Add"}
+            <button className=" bg-slate-500 px-4 py-2 w-36 text-lg text-white rounded-md ">
+              Cancel
+            </button>
+            <button
+              onClick={handleSubmit(submitProductForm)}
+              className=" bg-orange-500 px-4 py-2 w-36 text-lg text-white rounded-md "
+            >
+              {Object.keys(editProduct).length ? "Update" : "Add"}
             </button>
           </div>
-
-        </form>
+        </div>
       </div>
     </div>
   );
--- src/components/Products/AddProductDetails.jsx
+++ src/components/Products/AddProductDetails.jsx
@@ -1,4 +1,16 @@
-const AddProductDetails = ({closeModal}) => {
+import { useForm } from "react-hook-form";
+
+const AddProductDetails = ({ closeModal }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitProductDetailsForm = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className="fixed flex z-50 justify-center items-center py-4 top-0 left-0 bg-black/25 w-full h-full">
       <div className="flex flex-col p-4 bg-white w-[900px] h-auto rounded-md">
@@ -22,92 +34,113 @@ const AddProductDetails = ({closeModal}) => {
             />
           </svg>
         </div>
-        <form className="px-4 flex flex-col gap-3" action="">
-          <div className="flex p-4 w-[850px items-center justify-between">
+        <div className="px-4 flex flex-col gap-3">
+          <div className="flex w-[850px items-center justify-between">
             <div className="flex flex-col  ">
               <label className="text-lg font-thin mb-2" htmlFor="weight">
                 Weight (kg)
               </label>
               <input
+                {
+                  ...register("weight",{
+                    required: "Weight is required"
+                  })
+                }
                 className=" w-96 border border-gray-400 px-2 h-12 outline-none rounded-md"
                 type="number"
                 placeholder="e.g. 10kg"
               />
+              <span className="text-red-500">
+                {errors?.weight && errors?.weight?.message}
+              </span>
             </div>
             <div className="flex flex-col  ">
               <label className="text-lg font-thin mb-2" htmlFor="quantity">
                 Quantity
               </label>
               <input
+              {
+                ...register("quantity", {
+                  required: "Quantity is required"
+                })
+              }
                 className="w-96 border border-gray-400 px-2 h-12 outline-none rounded-md"
                 type="number"
                 placeholder="2"
               />
+              <span className="text-red-500">
+                {errors?.quantity && errors?.quantity?.message}
+              </span>
             </div>
           </div>
-          <div className="flex p-4 w-[850px] items-center justify-between">
+          <div className="flex w-[850px] items-center justify-between">
             <div className="flex flex-col  ">
               <label className="text-lg font-thin mb-2" htmlFor="cost price">
                 Cost Price (&#8373;)
               </label>
               <input
+              {
+                ...register("cost_price", {
+                  required: "Cost price is required"
+                })
+              }
                 className="w-48 border border-gray-400 px-2 h-12 outline-none rounded-md"
                 type="number"
                 placeholder="100.00"
               />
+              <span className="text-red-500">
+                {errors?.cost_price && errors?.cost_price?.message}
+              </span>
             </div>
             <div className="flex flex-col  ">
               <label className="text-lg font-thin mb-2" htmlFor="unit price">
                 Unit Price (&#8373;)
               </label>
               <input
+              {
+                ...register("unit_price", {
+                  required: "Enter unit price"
+                })
+              }
                 className="w-48 border border-gray-400 px-2 h-12 outline-none rounded-md"
+                type="number"
+                placeholder="20.00"
+              />
+              <span className="text-red-500">
+                {errors?.unit_price && errors?.unit_price?.message}
+              </span>
+            </div>
+            <div className="flex flex-col  ">
+              <label className="text-lg font-thin mb-2" htmlFor="Total Income">
+                Total Income (&#8373;)
+              </label>
+              <input
+                className="bg-lime-700 text-white w-48 border border-gray-400 px-2 h-12 outline-none rounded-md"
                 type="number"
                 placeholder="20.00"
               />
             </div>
             <div className="flex flex-col  ">
-            <label className="text-lg font-thin mb-2" htmlFor="Total Income">
-              Total Income (&#8373;)
-            </label>
-            <input
-              className="bg-lime-700 text-white w-48 border border-gray-400 px-2 h-12 outline-none rounded-md"
-              type="number"
-              placeholder="20.00"
-            />
+              <label className="text-lg font-thin mb-2" htmlFor="profit">
+                Profit (&#8373;)
+              </label>
+              <input
+                className="w-48 text-white border border-gray-400 px-2 h-12 outline-none rounded-md bg-lime-700  "
+                type="number"
+                placeholder="20.00"
+              />
+            </div>
           </div>
-          <div className="flex flex-col  ">
-            <label className="text-lg font-thin mb-2" htmlFor="profit">
-              Profit (&#8373;)
-            </label>
-            <input
-              className="w-48 text-white border border-gray-400 px-2 h-12 outline-none rounded-md bg-lime-700  "
-              type="number"
-              placeholder="20.00"
-            />
-          </div>
-          </div>
-          
 
-          <div className="flex flex-col  ">
-            <label className="text-lg font-thin mb-2" htmlFor="created by">
-              Created by:
-            </label>
-            <input
-              className="border border-gray-400 px-2 h-12 outline-none rounded-md"
-              type="text"
-              placeholder="Kwaw"
-            />
-          </div>
           <div className="flex gap-4 py-4">
             <button className="bg-slate-500 rounded-md w-36 px-4 py-2 text-lg text-white">
               Cancel
             </button>
-            <button className="bg-orange-500 rounded-md text-lg text-white px-4 py-2 w-36">
+            <button onClick={handleSubmit(submitProductDetailsForm)} className="bg-orange-500 rounded-md text-lg text-white px-4 py-2 w-36">
               Add
             </button>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
